aab150b7dad906e78f202f8e97ee42f775db3b00
heru-sn <mail.snru@gmail.com>
2026-02-02 23:59:26 +0700

    category layer architecture

--- main.go
+++ main.go
@@ -10,111 +10,11 @@ import (
 	"kasir-api/handlers"
 	"net/http"
 	"os"
-	"strconv"
 	"strings"
 	"log"
 
 	"github.com/spf13/viper"
 )
-
-// Category represents a product in the cashier system
-type Category struct {
-	ID    int          `json:"id"`
-	Name  string 	   `json:"name"`
-	Description string `json:"description"`
-}
-
-// In-memory storage (sementara, nanti ganti database)
-var category = []Category{
-	{ID: 1, Name: "Makanan Instan", Description: "Produk siap saji atau mudah dimasak"},
-	{ID: 2, Name: "Minuman", Description: "Produk minuman kemasan"},
-	{ID: 3, Name: "Bumbu Dapur", Description: "Bahan pelengkap dan penyedap masakan"},
-}
-
-func getCategoriesByID(w http.ResponseWriter, r *http.Request) {
-	// Parse ID dari URL path
-	// URL: /api/produk/123 -> ID = 123
-	idStr := strings.TrimPrefix(r.URL.Path, "/categories/")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "Invalid Category ID", http.StatusBadRequest)
-		return
-	}
-
-	// Cari category dengan ID tersebut
-	for _, p := range category {
-		if p.ID == id {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(p)
-			return
-		}
-	}
-
-	// Kalau tidak found
-	http.Error(w, "Category belum ada", http.StatusNotFound)
-}
-
-func updateCategories(w http.ResponseWriter, r *http.Request) {
-	// get id dari request
-	idStr := strings.TrimPrefix(r.URL.Path, "/categories/")
-
-	// ganti int
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "Invalid Category ID", http.StatusBadRequest)
-		return
-	}
-
-	// get data dari request
-	var updateCategories Category
-	err = json.NewDecoder(r.Body).Decode(&updateCategories)
-	if err != nil {
-		http.Error(w, "Invalid request", http.StatusBadRequest)
-		return
-	}
-
-	// loop category, cari id, ganti sesuai data dari request
-	for i := range category {
-		if category[i].ID == id {
-			updateCategories.ID = id
-			category[i] = updateCategories
-
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(updateCategories)
-			return
-		}
-	}
-	
-	http.Error(w, "Category belum ada", http.StatusNotFound)
-}
-
-func deleteCategories(w http.ResponseWriter, r *http.Request) {
-	// get id
-	idStr := strings.TrimPrefix(r.URL.Path, "/categories/")
-	
-	// ganti id int
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, "Invalid Category ID", http.StatusBadRequest)
-		return
-	}
-	
-	// loop category cari ID, dapet index yang mau dihapus
-	for i, p := range category {
-		if p.ID == id {
-			// bikin slice baru dengan data sebelum dan sesudah index
-			category = append(category[:i], category[i+1:]...)
-			
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(map[string]string{
-				"message": "sukses delete",
-			})
-			return
-		}
-	}
-
-	http.Error(w, "Category belum ada", http.StatusNotFound)
-}
 
 type Config struct {
 	Port    string `mapstructure:"PORT"`
@@ -146,51 +46,16 @@ func main() {
 	productService := services.NewProductService(productRepo)
 	productHandler := handlers.NewProductHandler(productService)
 
+	categoryRepo := repositories.NewCategoryRepository(db)
+	categoryService := services.NewCategoryService(categoryRepo)
+	categoryHandler := handlers.NewCategoryHandler(categoryService)
+
 	// setup routes
 	http.HandleFunc("/api/produk", productHandler.HandleProducts)
 	http.HandleFunc("/api/produk/", productHandler.HandleProductsByID)
 
-	// >>>>> CATEGORIES 
-
-	// GET localhost:8080/categories{id}
-	// PUT localhost:8080/categories{id}
-	// DELETE localhost:8080/categories{id}
-	http.HandleFunc("/categories/", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			getCategoriesByID(w, r)
-		} else if r.Method == "PUT" {
-			updateCategories(w, r)
-		} else if r.Method == "DELETE" {
-			deleteCategories(w, r)
-		}
-	})
-
-	// GET localhost:8080/categories
-	// POST localhost:8080/categories
-	http.HandleFunc("/categories", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == "GET" {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(category)
-			
-		} else if r.Method == "POST" {
-			// baca data dari request
-			var categoryBaru Category
-			err := json.NewDecoder(r.Body).Decode(&categoryBaru)
-			if err != nil {
-				http.Error(w, "Invalid request", http.StatusBadRequest)
-				return
-			}
-
-			// masukkin data ke dalam variable produk
-			categoryBaru.ID = len(category) + 1
-			category = append(category, categoryBaru)
-
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusCreated) // 201
-			json.NewEncoder(w).Encode(categoryBaru)
-		}
-	})
-
+	http.HandleFunc("/category", categoryHandler.HandleCategories)
+	
 	// localhost:8080/health
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
---
+++ repositories/category_repository.go
@@ -0,0 +1,95 @@
+package repositories
+
+import (
+	"database/sql"
+	"kasir-api/models"
+	"errors"
+)
+
+type CategoryRepository struct {
+	db *sql.DB
+}
+
+func NewCategoryRepository(db *sql.DB) *CategoryRepository {
+	return &CategoryRepository{db: db}
+}
+
+func (repo *CategoryRepository) GetAll() ([]models.Category, error) {
+	query := "SELECT id, name, description FROM category"
+	rows, err := repo.db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	products := make([]models.Category, 0)
+	for rows.Next() {
+		var p models.Category
+		err := rows.Scan(&p.ID, &p.Name, &p.Description)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+
+	return products, nil
+}
+
+func (repo *CategoryRepository) Create(category *models.Category) error {
+	query := "INSERT INTO category (name, description) VALUES ($1, $2) RETURNING id"
+	err := repo.db.QueryRow(query, category.Name, category.Description).Scan(&category.ID)
+	return err
+}
+
+// GetByID - ambil category by ID
+func (repo *CategoryRepository) GetByID(id int) (*models.Category, error) {
+	query := "SELECT id, name, description FROM category WHERE id = $1"
+
+	var c models.Category
+	err := repo.db.QueryRow(query, id).Scan(&c.ID, &c.Name, &c.Description)
+	if err == sql.ErrNoRows {
+		return nil, errors.New("category tidak ditemukan")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+func (repo *CategoryRepository) Update(category *models.Category) error {
+	query := "UPDATE category SET name = $1, description = $2 WHERE id = $3"
+	result, err := repo.db.Exec(query, category.Name, category.Description, category.ID)
+	if err != nil {
+		return err
+	}
+
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return errors.New("category tidak ditemukan")
+	}
+
+	return nil
+}
+
+func (repo *CategoryRepository) Delete(id int) error {
+	query := "DELETE FROM category WHERE id = $1"
+	result, err := repo.db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if rows == 0 {
+		return errors.New("category tidak ditemukan")
+	}
+
+	return err
+}
